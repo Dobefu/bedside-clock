b711f2cb0dea0293f630ef4a10175b6274658c2f
Connor van Spronssen <info@dobefu.com>
2024-08-16 16:27:31 +0200

    Change of plans, going bare metal

--- README.md
+++ README.md
@@ -4,13 +4,10 @@ Just my personal clock software, hosted on a Raspberry Pi
 
 ## Requirements
 
-- CMake
-- GNU Make
-- CLang compiler
-- XLib
-- Some X server (Quartz on MacOS, X11 on Linux)
+- `gcc`
+- `aarch-elf-gcc`
+- `qemu` (Optional, for testing)
 
-## Installation
+## Compiling
 
-- `cmake .`
 - `make`
--- main.c
+++ main.c
@@ -1,60 +1,39 @@
-#include <X11/Xatom.h>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "mbox.h"
+#include "uart.h"
 
-#include "main.h"
-#include "window.c"
-
-struct WindowProps wp;
-
-Display *d;
-Window w;
-GC gc;
-int s;
-
-int main(void)
+int main()
 {
-  wp.height = 480;
-  wp.width = 800;
+  // set up serial console
+  uart_init();
 
-  XEvent e;
+  // get the board's unique serial number with a mailbox call
+  mbox[0] = 8 * 4;        // length of the message
+  mbox[1] = MBOX_REQUEST; // this is a request message
 
-  d = XOpenDisplay(NULL);
+  mbox[2] = MBOX_TAG_GETSERIAL; // get serial number command
+  mbox[3] = 8;                  // buffer size
+  mbox[4] = 8;
+  mbox[5] = 0; // clear output buffer
+  mbox[6] = 0;
 
-  if (d == NULL)
+  mbox[7] = MBOX_TAG_LAST;
+
+  // send the message to the GPU and receive answer
+  if (mbox_call(MBOX_CH_PROP))
   {
-    fprintf(stderr, "Cannot open display\n");
-    return 1;
+    uart_puts("My serial number is: ");
+    uart_hex(mbox[6]);
+    uart_hex(mbox[5]);
+    uart_puts("\n");
+  }
+  else
+  {
+    uart_puts("Unable to query serial!\n");
   }
 
-  create_window(d, &w, s, wp);
-
-  // Create a graphics context.
-  gc = XCreateGC(d, w, 0, 0);
-
-  // Set up input.
-  XSelectInput(d, w, ExposureMask | KeyPressMask);
-  XMapWindow(d, w);
-
+  // echo everything back
   while (1)
   {
-    XNextEvent(d, &e);
-
-    if (e.type == Expose)
-    {
-      draw(d, w, s, wp, &gc);
-    }
-
-    // Close the window when escape is pressed.
-    if (e.type == KeyPress && e.xkey.keycode == 61)
-    {
-      break;
-    }
+    uart_send(uart_getc());
   }
-
-  XCloseDisplay(d);
-  return 0;
 }
